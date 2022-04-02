8230e67f81d382a28c0537aaca2911d8cc4cc1b1
arathorn2 <efohughes@gmail.com>
2022-04-02 08:45:05 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "Starships"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+libraryDependencies += "org.processing" % "core" % "3.3.6"
---
+++ src/main/scala/Starships.scala
@@ -0,0 +1,21 @@
+import processing.core._
+import processing.event.MouseEvent
+
+class Starships extends PApplet{
+  override def setup(): Unit = {}
+
+  override def settings(): Unit = {
+    size(1024, 512)
+    noSmooth()
+  }
+
+  override def draw(): Unit = {
+    background(10, 10, 10)
+
+  }
+
+}
+object Starships extends App {
+  PApplet.main(classOf[Starships])
+
+}
