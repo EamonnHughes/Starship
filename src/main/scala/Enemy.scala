d50750c6b7992c85f8d96687f3a940a7d979248a
arathorn2 <efohughes@gmail.com>
2022-04-09 14:41:35 -0400

    /

--- src/main/scala/Enemy.scala
+++ src/main/scala/Enemy.scala
@@ -1,8 +1,15 @@
 import processing.core.PApplet
 
-case class Enemy(var x: Float, var y: Float) extends Scrolling {
+case class Enemy(
+    var x: Float,
+    var y: Float,
+    var velocity: Float,
+    var deceleration: Float,
+    var health: Int
+) extends Scrolling {
   def draw(p: PApplet): Unit = {
     p.fill(75, 175, 25)
     p.ellipse(x, y, 20, 20)
   }
+
 }
