c1b62ce17eda9195bef46ed0b66a8ca377895a90
arathorn2 <efohughes@gmail.com>
2022-04-10 11:44:06 -0400

    Update Spawner.scala

--- src/main/scala/Spawner.scala
+++ src/main/scala/Spawner.scala
@@ -1,5 +1,11 @@
 object Spawner {
-  def checkForSpawn(): Unit = {}
-  def spawnEnemies(): Unit = {}
+  def checkForSpawn(): Unit = {
+    if (World.enemies.length < 3) {
+      spawnEnemies()
+    }
+  }
+  def spawnEnemies(): Unit = {
+    World.enemies = Enemy(1000, 256, 0, 0.9f, 3) :: World.enemies
+  }
   def spawnWalls(): Unit = {}
 }
