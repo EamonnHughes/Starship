8706b21e5a16a1b0a7a08255314887453ae6f20f
arathorn2 <efohughes@gmail.com>
2022-04-12 20:09:38 -0400

    Update Laser.scala

--- src/main/scala/Laser.scala
+++ src/main/scala/Laser.scala
@@ -5,7 +5,7 @@ case class Laser(var x: Float, var y: Float) extends Actor with Projectile {
   var time: Long = System.currentTimeMillis
   def draw(p: PApplet): Unit = {
     p.fill(255, 0, 0)
-    p.rect(World.player.x, World.player.y + 9, 1024 - x, 20)
+    p.rect(World.player.x, World.player.y + 9, 1024 - x, 2s)
   }
 
   def update(): Unit = {
