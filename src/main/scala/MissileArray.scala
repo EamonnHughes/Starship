1923f8189694a04cdfb2285988825e7e4874f3aa
arathorn2 <efohughes@gmail.com>
2022-04-13 21:05:44 -0400

    Create MissileArray.scala

---
+++ src/main/scala/MissileArray.scala
@@ -0,0 +1,20 @@
+import processing.core.PApplet
+
+case class MissileArray(var fireRate: Int, var damage: Int) extends Weapon {
+
+  var time: Long = System.currentTimeMillis
+  def shoot(): Unit = {
+    val currentTime = System.currentTimeMillis
+
+    if (currentTime > time + fireRate) {
+      World.projectilesList = MachineGunProjectile(
+        World.player.x + 25,
+        World.player.y + 10,
+        1
+      ) :: World.projectilesList
+      time = currentTime
+    }
+  }
+  def special(): Unit = {}
+  def drawPoints(p: PApplet): Unit = {}
+}
