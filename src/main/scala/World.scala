f0d720f1476640acda154eface35778909c9d682
arathorn2 <efohughes@gmail.com>
2022-04-13 21:00:33 -0400

    w

--- src/main/scala/World.scala
+++ src/main/scala/World.scala
@@ -1,10 +1,9 @@
 object World {
-  var player = Player(64, 256, 0, 0.9f, 3, MiningLaser(50, 1, 0))
+  var player = Player(64, 256, 0, 0.9f, 3, MachineGun(50, 1, 0))
   var projectilesList = List.empty[MachineGunProjectile]
   var walls = List.empty[Wall]
   var worldBorder = Border()
   var enemies = List.empty[Enemy]
-  var pLaser = Option.empty[Laser]
   def everything: List[Actor] = {
     player :: projectilesList ::: pLaser.toList ::: enemies
   }
