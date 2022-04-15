efa8cd625f067f4ba64c132fc395ec30fb9617a8
arathorn2 <efohughes@gmail.com>
2022-04-15 17:39:43 -0400

    Update Ancalagon.scala

--- src/main/scala/Ancalagon.scala
+++ src/main/scala/Ancalagon.scala
@@ -2,7 +2,10 @@ import processing.core.PApplet
 
 case class Ancalagon(var health: Int, var x: Float, var y: Float) extends Boss {
 
-  def draw(p: PApplet): Unit = {}
+  def draw(p: PApplet): Unit = {
+    p.fill(240, 100, 240)
+    p.rect(x, y, 40, 40)
+  }
 
   def update(): Unit = {}
 }
