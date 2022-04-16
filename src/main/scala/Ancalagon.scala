acc05ebc17a0aaff41c80a001195dfc454b1cc5a
arathorn2 <efohughes@gmail.com>
2022-04-16 08:21:59 -0400

    Update Ancalagon.scala

--- src/main/scala/Ancalagon.scala
+++ src/main/scala/Ancalagon.scala
@@ -2,10 +2,21 @@ import processing.core.PApplet
 
 case class Ancalagon(var health: Int, var x: Float, var y: Float) extends Boss {
 
+  var time: Long = System.currentTimeMillis
   def draw(p: PApplet): Unit = {
     p.fill(240, 100, 240)
     p.rect(x, y, 40, 40)
   }
 
-  def update(): Unit = {}
+  def update(): Unit = {
+
+    val currentTime = System.currentTimeMillis
+    if (currentTime > time + 900) {
+      World.projectilesList =
+        MachineGunProjectile(x - 25, y + 10, -1) :: World.projectilesList
+
+      time = currentTime
+
+    }
+  }
 }
