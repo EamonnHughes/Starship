daa16421625ff5a13621a392dd5b0b293c469001
arathorn2 <efohughes@gmail.com>
2022-04-17 12:52:42 -0400

    Update HealthUpgrade.scala

--- src/main/scala/HealthUpgrade.scala
+++ src/main/scala/HealthUpgrade.scala
@@ -5,9 +5,20 @@ case class HealthUpgrade(var location: Vec2, var size: Vec2)
     with Actor {
 
   def box: Box2 = Box2(location, size)
-  def update(): Unit = {}
+  def update(): Unit = {
+    if (World.player.box.intersects(box)) {
+      World.player.lives = clamp(World.player.lives + 1, 3).toInt
+
+    }
+  }
   def draw(p: PApplet): Unit = {
     p.fill(244, 244, 0)
     p.rect(location.x, location.y, size.x, size.y)
+  }
+
+  def clamp(value: Float, max: Float) = {
+    if (value > max) max
+    else if (value < -max) -max
+    else value
   }
 }
