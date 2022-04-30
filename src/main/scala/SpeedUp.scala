2635bed4fed5de006e60d6ab89e0e38d025dcca6
arathorn2 <efohughes@gmail.com>
2022-04-30 18:06:06 -0400

    e

--- src/main/scala/SpeedUp.scala
+++ src/main/scala/SpeedUp.scala
@@ -7,7 +7,7 @@ case class SpeedUp(var location: Vec2, var size: Vec2)
   def box: Box2 = Box2(location, size)
   def update(timeFactor: Float): Unit = {
     if (World.player.box.intersects(box)) {
-      Starships.scrollspeed = clamp(Starships.scrollspeed + 0.5f, 3f)
+      Starships.scrollspeed = clamp(Starships.scrollspeed + 0.3f, 3f)
       World.upgradeList =
         World.upgradeList.filterNot(upgrade => upgrade == this)
     }
