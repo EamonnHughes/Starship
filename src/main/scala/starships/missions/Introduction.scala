9de6fcfc8169e2a1db1a656390e55913050fe431
arathorn2 <efohughes@gmail.com>
2022-05-14 20:33:06 -0400

    .

---
+++ src/main/scala/starships/missions/Introduction.scala
@@ -0,0 +1,11 @@
+package starships.missions
+import starships.Starships
+import starships.geom._
+import starships.hostiles._
+import starships.obstacles._
+import starships.projectiles._
+import starships.traits._
+import starships.upgrades._
+import starships.weapons._
+import starships.world._
+class Introduction extends Mission {}
---
+++ src/main/scala/starships/traits/Mission.scala
@@ -0,0 +1,3 @@
+package starships.traits
+
+trait Mission {}
